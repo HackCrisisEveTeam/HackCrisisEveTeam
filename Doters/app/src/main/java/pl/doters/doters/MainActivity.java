2abd9e736f6ecbd83bff924137a91b2aae4a2991
Xsardas665 <Xsardas665@gmail.com>
2020-03-18 21:12:46 +0100

    Added Push Notifications

--- Doters/app/src/main/java/pl/doters/doters/MainActivity.java
+++ Doters/app/src/main/java/pl/doters/doters/MainActivity.java
@@ -3,8 +3,11 @@ package pl.doters.doters;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
@@ -14,6 +17,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -22,6 +27,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 
 import static java.lang.Math.PI;
@@ -50,6 +56,11 @@ public class MainActivity
     private TextView tvLon;
     private TextView tvLat;
 
+
+    private static final String CHANNEL_ID = "range_notifications";
+    private static final String CHANNEL_NAME = "Range Notifications";
+    private static final String CHANNEL_DESC = "Closing to Border Notifications";
+
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +80,8 @@ public class MainActivity
 
             if (googleServicesAvailable()) {
                 setContentView(R.layout.activity_main);
+                tvLat = findViewById(R.id.latitude);
+                tvLon = findViewById(R.id.longitude);
                 initMap();
             } else {
                 Log.e(TAG, "Nie Dostępne Usługi Google");
@@ -102,6 +115,7 @@ public class MainActivity
         return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
+    @SuppressLint("MissingPermission")
     private void getLocation() {
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (permissions) {
@@ -132,6 +146,19 @@ public class MainActivity
         setUpMap();
         goToLocationZoom();
         UpdateLocation();
+        drawCircles();
+    }
+    private void drawCircles() {
+        mGoogleMap.addCircle(new CircleOptions()
+                .center(new LatLng(cLatitude, cLongitude))
+                .radius(25)
+                .strokeColor(Color.RED));
+                //.fillColor(R.color.transparentRed));
+        mGoogleMap.addCircle(new CircleOptions()
+                .center(new LatLng(cLatitude, cLongitude))
+                .radius(10)
+                .strokeColor(Color.YELLOW));
+                //.fillColor(R.color.transparentYellow));
     }
     private void setUpMap() {
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
@@ -144,22 +171,42 @@ public class MainActivity
     }
     @SuppressLint("SetTextI18n")
     private void UpdateLocation() {
+        tvLon.setText("LongiTude : " + uLongitude);
+        tvLat.setText("Latitude : " + uLatitude);
         //noinspection deprecation
         mGoogleMap.setOnMyLocationChangeListener(location -> {
             uLatitude = location.getLatitude();
             uLongitude = location.getLongitude();
 
-            CameraUpdate center = CameraUpdateFactory.newLatLngZoom(new LatLng(uLatitude, uLongitude), 11);
+            CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(uLatitude, uLongitude));
             tvLon.setText("LongiTude : " + uLongitude);
             tvLat.setText("Latitude : " + uLatitude);
 
-            double resetDist = 50.00000000;
-            double warnDist = 25.00000000;
+            double resetDist = 25.00000000;
+            double warnDist = 10.00000000;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel =
+                        new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+                channel.setDescription(CHANNEL_DESC);
+                NotificationManager manager = getSystemService(NotificationManager.class);
+                if (manager != null) {
+                    manager.createNotificationChannel(channel);
+                }
+            }
+
+            String mTitle, mContent;
 
             if(distFromHome(uLatitude, uLongitude) >= resetDist) {
                 Log.i(TAG, "Aktywowano resetowanie punktów");
+                mTitle = "Oddaliłeś się z Domu.";
+                mContent = "Twoje zdobyte dzisiaj punkty Zostały Zresetowane.";
+                displayNotification(mTitle, mContent);
             } else if (distFromHome(uLatitude, uLongitude) >= warnDist) {
                 Log.i(TAG, "Aktywowano Ostrzeżenie");
+                mTitle = "Oddalasz się od Domu";
+                mContent = "Twoje Punkty z Dzisiaj zostaną zresetowane jeżeli oddalisz się za bardzo.";
+                displayNotification(mTitle, mContent);
             }
 
             mGoogleMap.moveCamera(center);
@@ -172,5 +219,16 @@ public class MainActivity
         double dist = sqrt( (uLatitude - x1) * (uLatitude - x1) + (cos((x1 * PI) / 180) * (uLongitude - y1) * (cos((x1 * PI) / 180) * (uLongitude - y1) )) );
         return dist * tmp1;
     }
+    private void displayNotification(String mTitle, String mContent) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_info)
+                        .setContentTitle(mTitle)
+                        .setContentText(mContent)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(1, mBuilder.build());
+
+    }
 }
