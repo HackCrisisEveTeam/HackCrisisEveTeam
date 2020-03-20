1127588e16954cf0fddca7385eba9831c6a2c606
Mateusz <tysioncszejset@gmail.com>
2020-03-21 00:15:27 +0100

    - bottom navigation menu - fragments for main views

---
+++ Doters/app/src/main/java/pl/doters/doters/fragments/CalendarFragment.java
@@ -0,0 +1,21 @@
+package pl.doters.doters.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import pl.doters.doters.R;
+
+public class CalendarFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.calendar_fragment_view, container, false);
+    }
+}
